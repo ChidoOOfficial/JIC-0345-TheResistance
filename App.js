cf80ac3c43848daf0d5ab220c1e0d8bf81f096b3
Grace Dwyer <graceyd@gatech.edu>
2021-02-02 15:39:31 -0500

    another commit

--- App.js
+++ App.js
@@ -5,18 +5,22 @@ import { createStackNavigator} from 'react-navigation-stack';
 
 import LoginPage from './pages/LoginPage';
 import LessonsPage from './pages/LessonsPage';
+import UserProfileListPage from './pages/UserProfileListPage';
+import QuizzesPage from './pages/QuizzesPage';
+
 //import all the screens we are going to switch
 const App = createStackNavigator({
       //Constant which holds all the screens like index of any book
       LoginPage: { screen: LoginPage },
       //First entry by default be our first screen if we do not define initialRouteName
       LessonsPage: { screen: LessonsPage },
+
+      UserProfileListPage: { screen: UserProfileListPage },
+
+      QuizzesPage : { screen: QuizzesPage },
     },
     {
       initialRouteName: 'LoginPage',
     }
 );
 export default createAppContainer(App);
-
-
-
