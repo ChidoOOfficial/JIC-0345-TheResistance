7fc3ea34404676268f675917413a423c16258b84
arivera0206 <64396721+arivera0206@users.noreply.github.com>
2021-02-03 02:32:35 -0500

    Added more buttons and stylized logout button

--- pages/SettingsPage.js
+++ pages/SettingsPage.js
@@ -7,9 +7,31 @@ export default class SettingsPage extends React.Component {
         const { navigate } = this.props.navigation;
         return (
             <View style={styles.screen}>
-                
-                <TouchableOpacity onPress={() =>navigate('LoginPage')}>
-                    <View style={{backgroundColor: "lightgray", alignItems: 'center', flexDirection: "row",
+            	<View style={styles.options}>
+                	<TouchableOpacity style={styles.optionBtn}>
+                    	<View style={{alignItems: 'center', flexDirection: "row",
+                        	justifyContent: 'center', padding:6}}>
+                        	<Text style={styles.optionText}>Change Profile Picture</Text>
+                    	</View>
+                	</TouchableOpacity>
+
+                	<TouchableOpacity style={styles.optionBtn}>
+                    	<View style={{alignItems: 'center', flexDirection: "row",
+                        	justifyContent: 'center', padding:6}}>
+                        	<Text style={styles.optionText}>Change Password</Text>
+                    	</View>
+                	</TouchableOpacity>
+
+                	<TouchableOpacity style={styles.optionBtn}>
+                    	<View style={{alignItems: 'center', flexDirection: "row",
+                        	justifyContent: 'center', padding:6}}>
+                        	<Text style={styles.optionText}>Submit Feedback</Text>
+                    	</View>
+                	</TouchableOpacity>
+
+                </View>
+                <TouchableOpacity onPress={() =>navigate('LoginPage')} style={styles.logoutBtn}>
+                    <View style={{alignItems: 'center', flexDirection: "row",
                         justifyContent: 'center', padding:6}}>
                         <Text style={styles.logout}>Log out</Text>
                     </View>
@@ -21,13 +43,42 @@ export default class SettingsPage extends React.Component {
 
 const styles = StyleSheet.create({
     screen: {
-        flex: 1,
+    	flex: 1,
         backgroundColor: 'white',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
+    },
+    options: {
+    	flex: 1,
+        backgroundColor: 'white',
+    },
+    optionBtn:{
+    	backgroundColor: 'lightgray',
+    	borderRadius: 5,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+
+    },
+    optionText: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: '800',
+        flexDirection: 'row'
+    },
+    logoutBtn:{
+        width: "80%",
+        backgroundColor: "#fb5b5a",
+        borderRadius: 25,
+        height: 50,
+        alignSelf: "center",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+        marginBottom: 20
     },
     logout: {
-        color: 'red',
+        color: 'white',
         fontSize: 20,
         fontWeight: '800',
         flexDirection: 'row'
