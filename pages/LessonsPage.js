2df5d47ce8977204d8d00b0cdf2e6413d4f2585d
rpatwardhan7 <56324218+rpatwardhan7@users.noreply.github.com>
2021-02-02 21:31:12 -0500

    Created buttons for each lesson's sub-topics. Changed page layout and design

--- pages/LessonsPage.js
+++ pages/LessonsPage.js
@@ -1,112 +1,153 @@
-import {StyleSheet, View, Text, TextInput, TouchableOpacity, Picker,ScrollView} from 'react-native';
+import {StyleSheet, View, Text, TextInput, TouchableOpacity, ScrollView, FlatList, Button, SafeAreaView} from 'react-native';
+import { Card } from 'react-native-paper';
+import GestureRecognizer from 'react-native-swipe-gestures';
 import React from "react";
 //import all the components we are going to use.
 
 export default class LessonsPage extends React.Component {
-    constructor(props) {
-        super(props)
-
+    constructor() {
+        super();
         this.state = {
-            phrases: '',
-            firstLanguage: 'java',
-            secondLanguage: 'js',
+            buttonName: ' '
         }
+        ;
     }
+    _onPressButton() {
+        alert('You tapped the button')
+    }
+
     render() {
-        const { navigate } = this.props.navigation;
         return (
-            <View style={{flex:1}}>
-                <ScrollView contentContainerStyle={styles.screen}>
+            <ScrollView contentContainerStyle={styles.screen}>
 
-                    <View style={{ marginLeft: 80, marginRight: 80, marginTop: 80, borderWidth: 5, borderColor: 'black',
-                        borderRadius: 15, width: 240}}>
-                        <Picker
-                            style={styles.picker} itemStyle={styles.pickerItem}
-                            selectedValue={this.state.phrases}
-                            onValueChange={(itemValue) => this.setState({language: itemValue})}
-                        >
-                            <Picker.Item label="PHRASES" value="cp1" />
-                            <Picker.Item label="PHRASES 1" value="cp2" />
-                            <Picker.Item label="PHRASES 2" value="cp3" />
-                        </Picker>
-                    </View>
-                    <View style={{ marginLeft: 80, marginRight: 80, marginTop: 30, borderWidth: 5, borderColor: 'black',
-                        borderRadius: 15, width: 240}}>
-                        <Picker
-                            style={styles.picker} itemStyle={styles.pickerItem}
-                            selectedValue={this.state.language}
-                            onValueChange={(itemValue) => this.setState({language: itemValue})}
-                        >
-                            <Picker.Item label="COLORS" value="colors1" />
-                            <Picker.Item label="COLORS 2" value="colors2" />
-                            <Picker.Item label="COLORS 3" value="colors3" />
-                        </Picker>
-                    </View>
-                    <View style={{ marginLeft: 80, marginRight: 80, marginTop: 30, borderWidth: 5, borderColor: 'black',
-                        borderRadius: 15, width: 240}}>
-                        <Picker
-                            style={styles.picker} itemStyle={styles.pickerItem}
-                            selectedValue={this.state.language}
-                            onValueChange={(itemValue) => this.setState({language: itemValue})}
-                        >
-                            <Picker.Item label="NUMBERS" value="numbers1" />
-                            <Picker.Item label="NUMBERS 1" value="numbers2" />
-                            <Picker.Item label="NUMBERS 2" value="numbers3" />
-                        </Picker>
-                    </View>
-                    <View style={{ marginLeft: 80, marginRight: 80, marginTop: 30, borderWidth: 5, borderColor: 'black',
-                        borderRadius: 15, width: 240}}>
-                        <Picker
-                            style={styles.picker} itemStyle={styles.pickerItem}
-                            selectedValue={this.state.language}
-                            onValueChange={(itemValue) => this.setState({language: itemValue})}
-                        >
-                            <Picker.Item label="CLOTHING" value="clothing1" />
-                            <Picker.Item label="CLOTHING 1" value="clothing2" />
-                            <Picker.Item label="CLOTHING 2" value="clothing3" />
-                        </Picker>
-                    </View>
-                    <View style={{ marginLeft: 80, marginRight: 80, marginTop: 30, borderWidth: 5, borderColor: 'black',
-                        borderRadius: 15, width: 240 }}>
-                        <Picker
-                            style={styles.picker} itemStyle={styles.pickerItem}
-                            selectedValue={this.state.language}
-                            onValueChange={(itemValue) => this.setState({language: itemValue})}
-                        >
-                            <Picker.Item label="HOUSEHOLD" value="household1" />
-                            <Picker.Item label="HOUSEHOLD 1" value="household2" />
-                            <Picker.Item label="HOUSEHOLD 2" value="household3" />
-                        </Picker>
-                    </View>
-                    <View style={{ marginLeft: 80, marginRight: 80, marginTop: 30, borderWidth: 5, borderColor: 'black',
-                        borderRadius: 15, width: 240 }}>
-                        <Picker
-                            style={styles.picker} itemStyle={styles.pickerItem}
-                            selectedValue={this.state.language}
-                            onValueChange={(itemValue) => this.setState({language: itemValue})}
-                        >
-                            <Picker.Item label="GRAMMAR" value="grammar1" />
-                            <Picker.Item label="GRAMMAR 1" value="grammar2" />
-                            <Picker.Item label="GRAMMAR 2" value="grammar3" />
-                        </Picker>
-                    </View>
-                </ScrollView>
-            </View>
+                <Text style={styles.logo}> LESSONS </Text>
+                <TextInput
+                    placeholder="            Search for lessons..."
+                    placeholderTextColor="black"
+                    style={{
+                        backgroundColor: 'grey',
+                        height: 30,
+                        width:300,
+                        fontSize:20,
+                        color: "black",
+                        borderRadius:80,
+                        borderBottomWidth:0.5,
+                        borderBottomColor: '#7d90a0',
+                        marginBottom: 20
+                    }}
+                />
+                    <Card style={{backgroundColor:"ivory", height:200, marginBottom:20}}>
+                        <Text style={styles.titleText}> COMMON PHRASES </Text>
+                        <TouchableOpacity style={styles.buttons} onPress={this._onPressButton}>
+                            <Text style={styles.normalText}> COMMON PHRASES 1</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttons} onPress={this._onPressButton}>
+                            <Text style={styles.normalText}> COMMON PHRASES 2</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.buttons}>
+                            <Text style={styles.normalText}> COMMON PHRASES 3</Text>
+                        </TouchableOpacity>
+                    </Card>
 
+                <Card style={{backgroundColor:"ivory", height:200, marginBottom:20}}>
+                    <Text style={styles.titleText}> COLORS </Text>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> COLORS 1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> COLORS 2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> COLORS 3</Text>
+                    </TouchableOpacity>
+                </Card>
+
+                <Card style={{backgroundColor:"ivory", height:200, marginBottom:20}}>
+                    <Text style={styles.titleText}> NUMBERS </Text>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> NUMBERS 1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> NUMBERS 2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> NUMBERS 3</Text>
+                    </TouchableOpacity>
+                </Card>
+
+                <Card style={{backgroundColor:"ivory", height:200, marginBottom:20}}>
+                    <Text style={styles.titleText}> CLOTHING </Text>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> CLOTHING 1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> CLOTHING 2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> CLOTHING 3</Text>
+                    </TouchableOpacity>
+                </Card>
+
+                <Card style={{backgroundColor:"ivory", height:200, marginBottom:20}}>
+                    <Text style={styles.titleText}> HOUSING </Text>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> HOUSING 1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> HOUSING 2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> HOUSING 3</Text>
+                    </TouchableOpacity>
+                </Card>
+
+                <Card style={{backgroundColor:"ivory", height:200, marginBottom:20}}>
+                    <Text style={styles.titleText}> GRAMMAR </Text>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> GRAMMAR 1</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> GRAMMAR 2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttons}>
+                        <Text style={styles.normalText}> GRAMMAR 3</Text>
+                    </TouchableOpacity>
+                </Card>
+            </ScrollView>
         );
     }
+
 }
 const styles = StyleSheet.create({
+    image: {
+        flex: 1,
+        aspectRatio: 1.5,
+        resizeMode: 'contain',
+
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 40,
+        backgroundColor: '#ecf0f1',
+    },
+    paragraph: {
+        margin: 24,
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#34495e',
+    },
     screen: {
         flexGrow: 1,
-        backgroundColor: 'aliceblue',
+        backgroundColor: 'floralwhite',
         alignItems: 'center',
     },
     logo:{
         fontWeight:"bold",
         fontSize:50,
         color:"black",
-        marginBottom:40,
         textDecorationLine: 'underline'
     },
     inputView:{
@@ -124,14 +165,16 @@ const styles = StyleSheet.create({
     },
     buttons: {
         fontWeight: 'bold',
-        backgroundColor: 'white',
+        backgroundColor: 'honeydew',
         alignItems: 'center',
         padding: 10,
         width: 340,
         borderRadius:80,
-        marginTop: 20,
         color:'black',
-        borderColor:'black'
+        borderColor:'black',
+        justifyContent: "center",
+        height: 50,
+        marginBottom:6
     },
     search: {
         backgroundColor: "grey",
@@ -158,4 +201,14 @@ const styles = StyleSheet.create({
         right: 10,
         fontSize: 20
     },
+    titleText: {
+        fontSize: 20,
+        fontWeight: "bold",
+        fontFamily: 'notoserif',
+        alignSelf: 'center',
+    },
+    normalText: {
+        color: "black",
+        fontSize: 20,
+    }
 });
