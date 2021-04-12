b565a4b12ea99b7bc070052c7a26d66be478e566
Grace Dwyer <graceyd@gatech.edu>
2021-04-12 13:48:45 -0400

    quizzes changes

--- App.js
+++ App.js
@@ -45,6 +45,7 @@ const App = createStackNavigator({
       LessonCourse: {screen: LessonCourse },
       TeacherResultsPage: {screen: TeacherResultsPage },
       QuizSelectPage : { screen: QuizSelectPage }
+
     },
     {
       initialRouteName: 'LoginPage',
